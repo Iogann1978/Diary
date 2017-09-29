395b9d929db7b0c0462f9eef0141fa9a4612fbe7
Козлов Антон Викторович <AKozlov2@alfabank.ru>
2017-09-29 09:46:55 +0300

    trfd

--- Diary.bat
+++ Diary.bat
@@ -1,2 +1,6 @@
+<<<<<<< HEAD
 @echo off
 java -jar Diary.jar diary.db3
+=======
+java -jar Diary.jar diary.db3
+>>>>>>> 27-09-2017
--- Work.bat
+++ Work.bat
@@ -1,2 +1,6 @@
+<<<<<<< HEAD
 @echo off
 java -jar Diary.jar work.db3
+=======
+java -jar Diary.jar work.db3
+>>>>>>> 27-09-2017
